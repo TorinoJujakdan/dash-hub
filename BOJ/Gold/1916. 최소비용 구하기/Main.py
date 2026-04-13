8769b35899e10ad2cdb56212e2afc81520e4533c
TorinoJujakdan <juinwork617@gmail.com>
2026-04-13 17:42:47 +0900

    [Gold V] Title: 최소비용 구하기, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/1916. 최소비용 구하기/Main.py
@@ -0,0 +1,42 @@
+import sys
+import heapq
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
+
+def dijkstra(start):
+    pq = [(0, start)]
+    dists = [INF] * (N + 1)
+    dists[start] = 0
+    
+    while pq:
+        dist, node = heappop(pq)
+        
+        if dists[node] < dist:
+            continue
+        
+        for next_dist, next_node in graph[node]:
+            new_dist = dist + next_dist
+            
+            if dists[next_node] <= new_dist:
+                continue
+            
+            dists[next_node] = new_dist
+            heappush(pq, (new_dist, next_node))
+            
+    
+    return dists
+    
+    
+N = int(input())
+M = int(input())
+INF = float(10e6)
+graph = [[] * (N+1) for _ in range(N+1)]
+for _ in range(M):
+    s, e, w = map(int, input().split())
+    graph[s].append((w, e))
+
+start, end = map(int, input().split())
+result = dijkstra(start)
+
+print(result[-1])
