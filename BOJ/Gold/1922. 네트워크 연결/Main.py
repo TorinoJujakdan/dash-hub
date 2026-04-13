8f29d43975d10241d178cfef10cde7a56df45417
TorinoJujakdan <juinwork617@gmail.com>
2026-04-13 17:14:44 +0900

    [Gold IV] Title: 네트워크 연결, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/1922. 네트워크 연결/Main.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+import heapq
+from heapq import heappop, heappush
+
+input = sys.stdin.readline 
+
+def prim(start):
+    pq = [(0, start)]
+    visited = [0] * (N+1)
+    min_w = 0
+    
+    while pq:
+        weight, node = heappop(pq)
+        
+        if visited[node]:
+            continue
+        
+        visited[node] = 1
+        min_w += weight
+        
+        for next_weight, next_node in graph[node]:
+            if visited[next_node]:
+                continue
+            
+            heappush(pq, (next_weight, next_node))
+    
+    return min_w
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(M)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+result = prim(1)
+
+print(result)
